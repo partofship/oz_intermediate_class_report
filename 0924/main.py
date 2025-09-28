da8a383f5b67e19238d57f53e75d1e586e10bd3d
hochang0819 <strongandrew@naver.com>
2025-09-28 21:40:44 +0900

    feat: 0928 2140 작성중...

---
+++ 0924/main.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+# D: 인터페이스
+class PizzaMasterCompany(ABC):
+    @abstractmethod
+    def slicemypizza():
+        
+
+# A: 클라이언트
+if __name__ == "__main__":
+
+
+# B: 팩토리
+
+# C: ConcreteCreator
+
+# D: 인터페이스
+class PizzaMaster(ABC):
